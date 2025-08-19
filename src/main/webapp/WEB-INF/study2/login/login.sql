27e8bf008b51cdade66000b6ce2996ba8e69b27b
hyunmo <lsiz8297@gmail.com>
2025-08-19 17:54:06 +0900

    20250819 17:50 friend로그인처리

---
+++ src/main/webapp/WEB-INF/study2/login/login.sql
@@ -0,0 +1,21 @@
+show tables;
+desc hoewon;
+SELECT *FROM hoewon;
+
+CREATE TABLE friend(
+	idx INT NOT NULL auto_increment PRIMARY KEY,
+	mid VARCHAR(20) NOT NULL ,
+	pwd VARCHAR(20) NOT NULL,
+	nickName VARCHAR(20) NOT NULL,
+	name VARCHAR(20) NOT NULL,
+	age INT DEFAULT 20,
+	gender CHAR(2) DEFAULT '여자',
+	address VARCHAR(30),
+	UNIQUE KEY(mid),
+	UNIQUE KEY(nickName)
+);
+
+DESC friend;
+DROP TABLE friend;
+INSERT INTO friend VALUE (DEFAULT, 'admin', '1234', '관리맨', '관리자', 33, '남자', '서울');
+SELECT *from friend;
